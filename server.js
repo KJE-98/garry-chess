e60ebf6f16d23a33beea7e64e860961b9a182c55
KJE-98 <>
2022-06-13 19:50:50 -0400

    try to remove proxy issue

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require('express');
 // get MongoDB driver connection
 const dbo = require('./db/conn');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
