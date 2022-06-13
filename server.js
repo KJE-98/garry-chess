b9649b8b4fa02c6d686e3c3178045bd7a597863c
KJE-98 <>
2022-06-13 19:25:48 -0400

    moves cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require('express');
 // get MongoDB driver connection
 const dbo = require('./db/conn');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const app = express();
 
